c8c87031949d389bd50e327c8b4b91df3a295f03
Rehoboth <rehobothi@yahoo.com>
2020-01-27 12:24:26 +0100

    Variables and Constants v2

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+
+fun main(args : Array<String>){
+    println("Hello Coders");
+
+    //Variables
+    var x = 15;
+    x = 30
+
+    //Variable with type assigned
+    var name : String = "wonderful";
+    println("Young John $name");
+
+    //Constants in Kotlin: "val"
+    val yt = 80;
+
+    println(yt);
+}
